5c0d591bc4e1f0ee5e8ac181b59f5c1dac1c6ee2
IgorGarciaCosta <igonildo7@gmail.com>
2021-08-07 15:28:41 -0300

    UI system and screens scripts - starting

---
+++ Assets/Scripts/IP_UI_Screen.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(CanvasGroup))]
+public class IP_UI_Screen : MonoBehaviour
+{
+    //variables
+    [Header("Main Properties")]
+    public Selectable m_StartSelectable;
+
+    [Header("Screen Events")]
+    public UnityEvent onScreenStart = new UnityEvent();
+    public UnityEvent onScreenClose = new UnityEvent();
+    
+    private Animator animator;
+
+    void Start(){
+        animator = GetComponent<Animator>();
+        if(m_StartSelectable){
+            EventSystem.current.SetSelectedGameObject(m_StartSelectable.gameObject);
+        }
+    }
+
+    public virtual void StartScreen(){
+        if(onScreenStart!=null){
+            onScreenStart.Invoke();
+        }
+        handleAnimator("show");
+        
+    } 
+    public virtual void CloseScreen(){
+        if(onScreenClose!=null){
+            onScreenClose.Invoke();
+        }
+        handleAnimator("hide");
+    } 
+
+    void handleAnimator(string aTrigger){
+        if(animator){
+            animator.SetTrigger(aTrigger);
+        }
+    }
+
+    
+}
+
---
+++ Assets/Scripts/IP_UI_System.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+    public class IP_UI_System : MonoBehaviour
+    {
+        //variables
+        [Header("System events")]
+        public UnityEvent onSwitchedScreen = new UnityEvent();
+        private Component[] screens = new Component[0];
+
+        private IP_UI_Screen previousScreen;
+        public IP_UI_Screen PreviousScreen{get{return previousScreen;}}
+
+        private IP_UI_Screen currentScreen;
+        public IP_UI_Screen CurrentScreen{get{return currentScreen;}}
+        
+
+        //functions
+        void Start()
+        {
+            screens = GetComponentsInChildren<IP_UI_Screen>(true);
+        }
+
+        
+        void Update()
+        {
+            
+        }
+
+        public void SwitchScreens(IP_UI_Screen aScreen){
+            if(aScreen){
+                if(currentScreen){
+                    //currentScreen.Close()
+                    previousScreen = currentScreen;
+                }
+                currentScreen = aScreen;
+                currentScreen.gameObject.SetActive(true);
+                //currentScreen.StartScreen();
+
+                if(onSwitchedScreen!=null){
+                    onSwitchedScreen.Invoke();//broadcast triggered event 
+                }
+
+            }
+        }
+
+        public void GoToPreviousScreen(){
+            if(previousScreen){
+                SwitchScreens(previousScreen);
+            }
+        }
+
+        public void LoadScene(int sceneIndex){
+            StartCoroutine(WaitToLoad(sceneIndex));
+        }
+
+        IEnumerator WaitToLoad(int sceneIndex){
+            yield return null;
+        }
+    }
+
+
