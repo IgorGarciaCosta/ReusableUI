3bed1a30766f53bcfacea1eb583072e3a1b49314
IgorGarciaCosta <igonildo7@gmail.com>
2021-08-07 19:27:13 -0300

    Adding fader image

--- Assets/Scripts/IP_UI_System.cs
+++ Assets/Scripts/IP_UI_System.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Events;
 
 
@@ -10,6 +11,12 @@ using UnityEngine.Events;
         [Header("System events")]
         public UnityEvent onSwitchedScreen = new UnityEvent();
         private Component[] screens = new Component[0];
+
+        [Header("Fader Properties")]
+        public Image m_Fader;
+        public float m_FadeInDuration = 1f;
+        public float m_FadeOutDuration = 1f;
+
 
         private IP_UI_Screen previousScreen;
         public IP_UI_Screen PreviousScreen{get{return previousScreen;}}
@@ -22,6 +29,24 @@ using UnityEngine.Events;
         void Start()
         {
             screens = GetComponentsInChildren<IP_UI_Screen>(true);
+
+            if(m_Fader){
+                m_Fader.gameObject.SetActive(true);//initializes fader
+            }
+            FadeIn();
+        
+        }
+
+        public void FadeIn(){
+            if(m_Fader){
+                m_Fader.CrossFadeAlpha(0f, m_FadeInDuration, false);
+            }
+        }
+
+        public void FadeOut(){
+            if(m_Fader){
+                m_Fader.CrossFadeAlpha(0f, m_FadeOutDuration, false);
+            }
         }
 
         
